0dff6ea7b24cf1b95504ac6b9ce4d615e5e6bcd9
Shyam <srangana@redhat.com>
2015-10-19 15:54:31 -0400

    cluster/dht2: Prototype implementation of lookup FOP
    This is just a prototype implementation of lookup to
    exchange notes on DHT2 and POSIX2 development streams and to
    come to a consensus on how to proceed on certain areas.
    
    These sets of prototype commits, lead to an actual POC for DHT2,
    at which point, these would be moved into *real* production
    code.
    
    Change-Id: I504bab66fdb1b6177919809faee03ebf671a9daf
    Signed-off-by: Shyam <srangana@redhat.com>

--- xlators/cluster/dht2/src/dht2.h
+++ xlators/cluster/dht2/src/dht2.h
@@ -15,13 +15,48 @@
 #ifndef _DHT2_H
 #define _DHT2_H
 
+#include "iatt.h"
+#include "common-utils.h"
+#include "xlator.h"
 #include "dht2-mem-types.h"
 
-struct dht2_conf {
-        int     d2cnf_data_count;
-        int     d2cnf_mds_count;
-};
+#define DHT2_MSG_DOM "dht2"
+#define DHT_EREMOTE_XATTR_STR "eremote"
 
+typedef enum dht2_eremote_reasons {
+        DHT2_INODE_REMOTE = 1,
+        DHT2_UNKNOWN = 2
+} dht2_eremote_reasons_t;
+
+struct dht2_conf {
+        /* memory pools */
+        struct mem_pool *d2cnf_localpool;   /* mem pool for struct dht2_local
+                                           allocation */
+
+        /* subvolume configuration */
+        int     d2cnf_data_count;       /* count of data subvolumes */
+        int     d2cnf_mds_count;        /* count of meta-data subvolumes */
+
+        /* layout specification */
+        struct  dht2_layout *d2cnf_layout;
+
+        /* xattr specification */
+        char    *d2cnf_xattr_base_name; /* xattr base name for DHT2 xattrs */
+        char    *d2cnf_eremote_reason_xattr_name; /* xattr name for DHT2
+                                                   * EREMOTE error */
+};
 typedef struct dht2_conf dht2_conf_t;
+
+struct dht2_local {
+        glusterfs_fop_t      d2local_fop;
+        loc_t                d2local_loc;
+        dict_t              *d2local_xattr_req;
+        gf_boolean_t         d2local_postparent_stbuf_filled;
+        struct iatt          d2local_postparent_stbuf;
+};
+typedef struct dht2_local dht2_local_t;
+
+/* xlator FOP entry/exit points */
+int32_t dht2_lookup (call_frame_t *, xlator_t *, loc_t *, dict_t *);
 
 #endif /* _DHT2_H */
