9a55dc2e99cfc5aff8442ed047e7c87ed3a2dc32
Shyam <srangana@redhat.com>
2015-10-19 15:54:31 -0400

    cluster/dht2: Prototype implementation of lookup and stat FOPs
    This is just a prototype implementation of lookup and stat to
    exchange notes on DHT2 and POSIX2 development streams and to
    come to a consensus on how to proceed on certain areas.
    
    The intention further would be to use the code generation
    techniquies like NSR to generate swathes of DHT2 FOPs, both
    to speed up the development and also the accrue the benefits
    of the code generation method of writing xlators.
    
    Change-Id: I504bab66fdb1b6177919809faee03ebf671a9daf
    Signed-off-by: Shyam <srangana@redhat.com>

--- libglusterfs/src/glfs-message-id.h
+++ libglusterfs/src/glfs-message-id.h
@@ -155,6 +155,9 @@ GLFS_MSGID_COMP_SYMLINK_CACHE_END
 #define GLFS_MSGID_COMP_WRITE_BEHIND_END        (GLFS_MSGID_COMP_WRITE_BEHIND+\
                                                 GLFS_MSGID_SEGMENT)
 
+#define GLFS_MSGID_COMP_DHT2      GLFS_MSGID_COMP_WRITE_BEHIND_END
+#define GLFS_MSGID_COMP_DHT2_END  (GLFS_MSGID_COMP_DHT2 + GLFS_MSGID_SEGMENT)
+
 /* --- new segments for messages goes above this line --- */
 
 #endif /* !_GLFS_MESSAGE_ID_H_ */
---
+++ xlators/cluster/dht2/src/dht2-messages.h
@@ -0,0 +1,38 @@
+/*Copyright (c) 2015 Red Hat, Inc. <http://www.redhat.com>
+  This file is part of GlusterFS.
+
+  This file is licensed to you under your choice of the GNU Lesser
+  General Public License, version 3 or any later version (LGPLv3 or
+  later), or the GNU General Public License, version 2 (GPLv2), in all
+  cases as published by the Free Software Foundation.
+*/
+
+#ifndef _DHT2_MESSAGES_H_
+#define _DHT2_MESSAGES_H_
+
+#include "glfs-message-id.h"
+
+/*! \file dht2-messages.h
+ *  \brief DHT2 log-message IDs and their descriptions
+ *
+ */
+
+#define GLFS_DHT2_BASE          GLFS_MSGID_COMP_DHT2
+#define GLFS_DHT2_NUM_MESSAGES  4
+#define GLFS_MSGID_DHT2_END     (GLFS_DHT2_BASE + GLFS_DHT2_NUM_MESSAGES + 1)
+
+/*!
+ * @messageid 109001 (TBD: Correct this number)
+ * @diagnosis Entry name for object failed to return inode/gfid number
+ * @recommendedaction  None
+ *
+ */
+#define DHT2_MSG_INODE_NUMBER_MISSING (GLFS_DHT2_BASE + 1)
+
+#define DHT2_MSG_FIND_SUBVOL_ERROR (GLFS_DHT2_BASE + 2)
+
+#define DHT2_MSG_EREMOTE_REASON_MISSING (GLFS_DHT2_BASE + 3)
+
+#define DHT2_MSG_MISSING_GFID_IN_INODE (GLFS_DHT2_BASE + 4)
+
+#endif /* _DHT2_MESSAGES_H_ */
