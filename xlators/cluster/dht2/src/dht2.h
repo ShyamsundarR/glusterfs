06b7866355e0837f31b1a64c953a85cc0de8a9c6
Shyam <srangana@redhat.com>
2015-11-03 12:58:03 -0500

    cluster/dht2: Add prototype open/stat/setattr/flush FOPs
    Core code for the mentioned FOPs are added. This set is required
    for create to work from a FUSE mount as FUSE sends in these FOPs
    on a create.
    
    Quite a few inode based FOPs share the same pattern of code and
    hence would be moved to the code-gen facility, than repeating the
    code for each FOP.
    
    These sets of prototype commits, lead to an actual POC for DHT2,
    at which point, these would be moved into *real* production code.
    
    Change-Id: If6874c9aaa269d1f16e1f707edade5d116007c70
    Signed-off-by: Shyam <srangana@redhat.com>

--- xlators/cluster/dht2/src/dht2.h
+++ xlators/cluster/dht2/src/dht2.h
@@ -62,4 +62,14 @@ int32_t dht2_lookup (call_frame_t *, xlator_t *, loc_t *, dict_t *);
 int32_t dht2_create (call_frame_t *, xlator_t *, loc_t *, int32_t,
                      mode_t, mode_t, fd_t *, dict_t *);
 
+int32_t dht2_stat (call_frame_t *, xlator_t *, loc_t *, dict_t *);
+
+int32_t dht2_open (call_frame_t *, xlator_t *, loc_t *,
+                   int32_t, fd_t *, dict_t *);
+
+int32_t dht2_setattr (call_frame_t *, xlator_t *, loc_t *, struct iatt *,
+                      int32_t, dict_t *);
+
+int32_t dht2_flush (call_frame_t *, xlator_t *, fd_t *, dict_t *);
+
 #endif /* _DHT2_H */
