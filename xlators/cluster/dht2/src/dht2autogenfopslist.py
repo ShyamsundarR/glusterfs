edfd5a40f97db1e83f0de10a62aff3fb6b0df109
Shyam <srangana@redhat.com>
2015-12-03 14:58:00 -0500

    cluster/dht2: Implement directory listing support
    Adding directory listing support, via readdir only at this
    point. This needs opendir, release(dir) and readdir support
    which is added by this commit.
    
    TOBEREMOVED: Currently opendir is only implemented, and this helps
    create directories within directories, as parent diffd is used to
    create subdirectories
    
    Change-Id: I451972aab2cc8b758c5e9147a4a92e0ccccced67
    Signed-off-by: Shyam <srangana@redhat.com>

--- xlators/cluster/dht2/src/dht2autogenfopslist.py
+++ xlators/cluster/dht2/src/dht2autogenfopslist.py
@@ -4,8 +4,9 @@ import sys
 
 inode_ops = {'open', 'setattr', 'stat', 'access', 'truncate', 'readlink',
         'setxattr', 'getxattr', 'removexattr', 'inodelk', 'entrylk',
-        'xattrop'}
+        'xattrop', 'opendir'}
 fd_ops = {'fsetattr', 'fstat', 'ftruncate', 'fsetxattr', 'fgetxattr',
-        'fremovexattr', 'lk', 'finodelk', 'fentrylk', 'fxattrop', 'flush'}
+        'fremovexattr', 'lk', 'finodelk', 'fentrylk', 'fxattrop', 'flush',
+        'readdir'}
 unsup_ops = {'readdirp', 'rchecksum', 'getspec', 'fallocate', 'discard',
         'zerofill', 'ipc'}
