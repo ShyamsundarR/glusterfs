f1a970de3562e96f4bd69022c75c6172e6383964
Kotresh HR <khiremat@redhat.com>
2015-12-29 14:56:21 +0530

    posix2/ds: posix2_open
    Open the data file (represented by GFID of the file)
    and save the fd in fd context.
    
    Change-Id: I0d6f3225b081b7f05faa97d6a98b83041f467a0f
    Signed-off-by: Kotresh HR <khiremat@redhat.com>

--- xlators/storage/posix2/common/src/posix2-mem-types.h
+++ xlators/storage/posix2/common/src/posix2-mem-types.h
@@ -22,6 +22,7 @@ enum gf_posix2_mem_types {
 
 enum gf_posix2_ds_mem_types {
         gf_posix2_ds_mt_private_t = gf_posix2_mt_end + 1,
+        gf_posix2_ds_mt_posix2_fd_t,
         gf_posix2_ds_mt_end,
 };
 #endif /* __POSIX2_MEM_TYPES_H__ */
